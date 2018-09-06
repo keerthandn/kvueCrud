e9ade83e042e753e5ebd95997b1ecdaa1cd17ee7
keerthand <keerthand@atconline.biz>
2018-09-06 15:50:30 +0530

    update

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Data;
+
+class MainController extends Controller
+{
+    public function storeItem(Request $request)
+    {
+    	$data=new Data();
+    	$data->name=$request->name;
+    	$data->age=$request->age;
+    	$data->profession=$request->profession;
+    	$data->save();
+
+    	return $data;
+    }
+    public function getItems(Request $request)
+    {
+    	$data=Data::all();
+    	return $data;
+    }
+    public function deleteItem(Request $request)
+    {
+    	$data=Data::find($request->id)->delete();
+    }
+    public function editItem(Request $request,$id)
+    {
+    	$data=Data::where('id',$id)->first();
+
+    	$data->name=$request->get('val1');
+    	$data->age=$request->get('val2');
+    	$data->profession=$request->get('val3');
+    	$data->save();
+
+    	return $data;
+    }
+}
